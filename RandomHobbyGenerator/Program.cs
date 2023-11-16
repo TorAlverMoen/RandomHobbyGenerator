5da0ef71de0d26de095480caba3f36a9a82605ee
Tor Alver Moen <tor.alver.moen@gmail.com>
2023-11-16 10:24:22 +0100

    Initial commit

---
+++ RandomHobbyGenerator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RandomHobbyGenerator
+{
+    internal class Program
+    {
+        static string[] hobbies = { "frimerke samling", "fallskjerm hopping", "dypvannsfiske", "fotografering", "brodering av bunader" };
+
+        static int generateRandomIndex()
+        {
+            Random rndNum = new Random();
+            return rndNum.Next(0, 4);
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Hvem vil starte en ny hobby?");
+            var name = Console.ReadLine();
+            Console.WriteLine(name + " vil begynne med " + hobbies[generateRandomIndex()] + ".");
+        }
+    }
+}
